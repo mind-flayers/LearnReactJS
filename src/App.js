00e0b1f87df67b0241aa7476ff07132b81ca6986
Mind Flayer <mishaf1106@gmail.com>
2024-06-10 14:27:30 +0530

    Made some modification

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import "./App.css";
 const App = () => {
     return (
         <div className="App">
-            <Header />
+            <Header title="The Tasker App"/>
             <Content />
             <Footer />
         </div>
